3871cc06d3aff78d3dfe897ba1120e2261712140
Masoud S.R <amir.sepehr72@gmail.com>
2023-04-18 16:47:23 +0330

    winChecker code improvement & tie bug fixed

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const playerOne = [];
 const playerTwo = [];
 let playerTurn = 1;
-let filledBlocks = 0
+let filledBlocks = 0;
 
 const gameButtons = document.querySelectorAll(".btn");
-const retryBtn = document.querySelector(".retry-btn")
+const retryBtn = document.querySelector(".retry-btn");
 
-const tieHandler = ()=>{
+const tieHandler = () => {
 	document.querySelector("h2").innerText = `it's tied, no one wins`;
-    retryBtn.style.display="inline-block"
-}
+	retryBtn.style.display = "inline-block";
+};
 
 const gameButtonsHandler = (event) => {
-    filledBlocks++
-    event.target.removeEventListener("click" , gameButtonsHandler)
+	filledBlocks++;
+	event.target.removeEventListener("click", gameButtonsHandler);
 	if (playerTurn === 1) {
 		playerOne.push(Number(event.target.id));
 		event.target.innerText = "X";
@@ -25,78 +25,46 @@ const gameButtonsHandler = (event) => {
 		winChecker(playerTwo);
 		playerTurn = 1;
 	}
-    filledBlocks===9 ? tieHandler():null
+	filledBlocks === 9 ? tieHandler() : null;
 };
 
 const winHandler = (player) => {
 	document.querySelector("h2").innerText = `player ${playerTurn} win`;
-    gameButtons.forEach(btn => {
-        btn.removeEventListener("click" , gameButtonsHandler)
-    });
-    retryBtn.style.display="inline-block"
-    filledBlocks++
+	gameButtons.forEach((btn) => {
+		btn.removeEventListener("click", gameButtonsHandler);
+	});
+	retryBtn.style.display = "inline-block";
+	filledBlocks=0;
 };
 
 const winChecker = (player) => {
 	if (player.indexOf(5) > -1) {
-		if (player.indexOf(2) > -1) {
-			if (player.indexOf(8) > -1) {
-				winHandler();
-			}
-		}
-		if (player.indexOf(4) > -1) {
-			if (player.indexOf(6) > -1) {
-				winHandler();
-			}
-		}
-		if (player.indexOf(1) > -1) {
-			if (player.indexOf(9) > -1) {
-				winHandler();
-			}
-		}
-		if (player.indexOf(3) > -1) {
-			if (player.indexOf(7) > -1) {
-				winHandler();
-			}
-		}
+		player.indexOf(2) > -1 && player.indexOf(8) > -1 ? winHandler() : null;
+		player.indexOf(4) > -1 && player.indexOf(6) > -1 ? winHandler() : null;
+		player.indexOf(1) > -1 && player.indexOf(9) > -1 ? winHandler() : null;
+		player.indexOf(3) > -1 && player.indexOf(7) > -1 ? winHandler() : null;
 	}
 	if (player.indexOf(1) > -1) {
-		if (player.indexOf(2) > -1) {
-			if (player.indexOf(3) > -1) {
-				winHandler();
-			}
-		}
-		if (player.indexOf(4) > -1) {
-			if (player.indexOf(7) > -1) {
-				winHandler();
-			}
-		}
+		player.indexOf(2) > -1 && player.indexOf(3) > -1 ? winHandler() : null;
+		player.indexOf(4) > -1 && player.indexOf(7) > -1 ? winHandler() : null;
 	}
 	if (player.indexOf(9) > -1) {
-		if (player.indexOf(6) > -1) {
-			if (player.indexOf(3) > -1) {
-				winHandler();
-			}
-		}
-		if (player.indexOf(8) > -1) {
-			if (player.indexOf(7) > -1) {
-				winHandler();
-			}
-		}
+		player.indexOf(6) > -1 && player.indexOf(3) > -1 ? winHandler() : null;
+		player.indexOf(8) > -1 && player.indexOf(7) > -1 ? winHandler() : null;
 	}
 };
 
+const startGame = () => {
+	gameButtons.forEach((btn) => {
+		btn.addEventListener("click", gameButtonsHandler);
+		btn.innerText = "";
+		document.querySelector("h2").innerText = "";
+		playerOne.length = 0;
+		playerTwo.length = 0;
+		filledBlocks = 0;
+	});
+};
 
-const startGame= ()=>{
-    gameButtons.forEach(btn => {
-        btn.addEventListener("click", gameButtonsHandler);
-        btn.innerText=""
-	document.querySelector("h2").innerText = "";
-        playerOne.length=0
-        playerTwo.length=0
-    });
-}
+retryBtn.addEventListener("click", startGame);
 
-retryBtn.addEventListener("click" , startGame)
-
-startGame()
+startGame();
